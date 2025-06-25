0a115ebcbb769b508d8ff7ddc7f82a889094c2fd
Sachin Pal <sachinpal@Sachins-MacBook-Pro.local>
2025-06-25 21:32:24 +0530

    Lec 1: Things to Know in C++/Java/Python or any language

---
+++ Step-1: Learn the basics/L-1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /*
+        - User input/output
+        - Data Types
+          - int, long, long long, short, byte
+          - float, double
+          - char
+        - if else
+        - switch statments
+        - loops
+          - for loop
+          - while loop
+          - do-while loop
+        - Functions
+          - Pass by value -> doSomething(int x) -> normal
+          - Pass by refrence -> doSomething(int &x) -> passing address
+    */
+    return 0;
+}
